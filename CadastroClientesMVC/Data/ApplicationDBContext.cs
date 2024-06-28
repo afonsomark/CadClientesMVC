864038f0483c1db9e9a5cd8682e51bd744c22832
afonsomark <marcaf.sa@hotmail.com>
2024-06-28 17:30:45 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroClientesMVC/Data/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using CadastroClientesMVC.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CadastroClientesMVC.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ CadastroClientesMVC/Models/Entities/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace CadastroClientesMVC.Models.Entities
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Document { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
